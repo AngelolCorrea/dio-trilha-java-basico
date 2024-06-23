81eec9d7135f0b42501ed358796a1721482178ea
Angelo Correa <angeloicloudnovo@outlook.com>
2024-06-23 12:47:29 -0300

    Estrutura FOR array

---
+++ estruturas-repeticao/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+import java.awt.font.FontRenderContext;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"FELIPE", "JONAS","JULIA","MARCOS"};
+       /* for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice " +x +": " +alunos[x]);
+        }*/
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " +aluno);
+        }
+    }
+}
