fcbfea553cff8780d276db777adf49ef4f55dd79
Angelo Correa <angeloicloudnovo@outlook.com>
2024-06-23 11:20:03 -0300

    Exemplo estrutura condicional simples.

---
+++ controle-fluxo/src/CondicionalSimples.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CondicionalSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 25.00;
+        System.out.println("Bem vindo, seu saldo é: " +saldo);
+        System.out.println("Qual valor deseja sacar? ");
+        double valorSolicitado = scanner.nextDouble();
+
+            if (valorSolicitado < saldo)
+                saldo= saldo - valorSolicitado;
+                System.out.println("Seu novo saldo é: " +saldo);
+
+
+    }
+}
