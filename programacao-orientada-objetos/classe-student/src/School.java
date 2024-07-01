749f418e51ee63d71a55eba1fad11cd83e758b2e
Angelo Correa <angeloicloudnovo@outlook.com>
2024-07-01 19:42:26 -0300

    criação de pacotes

---
+++ programacao-orientada-objetos/classe-student/src/School.java
@@ -0,0 +1,5 @@
+public class School {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+    }
+}
---
+++ programacao-orientada-objetos/classe-student/src/Student.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Student {
+    String nome;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating (Food food){
+
+    }
+}
