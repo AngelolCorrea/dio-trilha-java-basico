d6a50e883d78c6cad69a99123be1152d283d0880
Angelo Correa <angeloicloudnovo@outlook.com>
2024-07-14 13:12:20 -0300

    heranca

--- programacao-orientada-objetos/conceitos-poo/src/ComputadorPedrinho.java
+++ programacao-orientada-objetos/conceitos-poo/src/ComputadorPedrinho.java
@@ -1,10 +1,18 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-        //abrindo messenger
+        System.out.println("MSN");
         MSNMessenger msn = new MSNMessenger();
-
         msn.enviarMensagem();
+        msn.receberMensagem();
 
-        msn.recebeMensagem();
+        System.out.println("Facebook");
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.receberMensagem();
+
+        System.out.println("Telegram");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
     }
 }
--- programacao-orientada-objetos/conceitos-poo/src/MSNMessenger.java
+++ programacao-orientada-objetos/conceitos-poo/src/MSNMessenger.java
@@ -1,19 +1,3 @@
-public class MSNMessenger {
-    public void enviarMensagem(){
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
-    }
+public class MSNMessenger extends ServicoMensagemInstantanea {
 
-    public void recebeMensagem(){
-        System.out.println("Recebendo mensagem");
-    }
-
-    private void validarConectadoInternet(){
-        System.out.println("Validando conexao com internet");
-    }
-
-    private void salvarHistoricoMensagem(){
-        System.out.println("Salvando histórico de mensagens");
-    }
 }
